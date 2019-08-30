1f6ccc4ab211e3ee9678e20da8a0b4191a04469f
Andres Quinto Quinto <andrquqn@suramericana.com.co>
2019-08-30 16:33:35 -0500

    Deploy a heroku

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const path = require('path');
+let express = require('express');
 const gateway = require('express-gateway');
+
+let router = express.Router();
 
 gateway().load(path.join(__dirname, 'config')).run();
